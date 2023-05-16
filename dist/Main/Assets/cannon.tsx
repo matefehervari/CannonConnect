5e5058942ccc354ff412350e9e9aae18faae38dd
Endoxide <38787928+endergamerplasma@users.noreply.github.com>
2023-05-17 00:29:22 +0100

    Cleaned pyinstaller files

--- dist/Main/Assets/cannon.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2021.01.13" name="cannon" tilewidth="170" tileheight="145" tilecount="4" columns="2">
- <image source="Assets/cannon.png" trans="ff00ff" width="340" height="325"/>
-</tileset>
